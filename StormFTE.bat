27450d4b800350e6d6a8e9a8b21617a55cbade50
Mick <mickkn@gmail.com>
2019-05-03 00:27:42 +0200

    Fixed bug in Obitary that crashed ezquke, missing message from RAILGUN kill. Thanks R00k @ Discord

---
+++ StormFTE.bat
@@ -0,0 +1,2 @@
+cd..
+fteqw64.exe -windows -mem 128 -zone 512 -dinput -m_smooth -bpp 32 +set cl_confirmquit 0 -noforceparms -cheats +cfg_load storm +cheats -dev -particles 80000 -condebug +sv_cheats 1 +crosshair 1 +gamedir storm +sv_gamedir storm +map storm_test +exec server.cfg +exec binds.cfg
